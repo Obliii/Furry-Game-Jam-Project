2be34db7f68c42a682a8b34c3a17df073788b7ad
Funce <funce.973@gmail.com>
2025-03-19 23:46:51 +1300

    couple of commments - oh and the name of our game is in the main menu now

--- UI/Scripts/main_menu.gd
+++ UI/Scripts/main_menu.gd
@@ -6,12 +6,12 @@ func _ready() -> void:
 	pass
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("uid://ellxio420wg0")
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("uid://ellxio420wg0") # level_0
 
 func _on_options_button_pressed() -> void:
 	pass # Replace with function body.
 
 func _on_quit_button_pressed() -> void:
 	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	# we gotta get outta here
 	get_tree().quit()
