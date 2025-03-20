6b9ba111d28aa95bfb26f999bfc191af1809e447
Lucas F <lucas.ferrier04@free.fr>
2025-03-20 18:28:31 +0100

    Camera Follow & Door Fix
    With smooth position

--- Scenes/Scripts/door.gd
+++ Scenes/Scripts/door.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 
 ## If the player enters, the door and the key disappears. This will be changed as soon as I get the assets!
 func _body_entered(body: Node2D):
-	if body.name == "Player":
+	if body.name == "BetaPlayer" or "AlphaPlayer":
 		queue_free()
 		if associated_key:
 			associated_key.queue_free()
