baebbd9b1066652933746a2ca5199c5db2ca5d3a
Obli <obliotter@gmail.com>
2025-03-20 01:25:02 -0500

    Update door.gd

--- Scenes/Scripts/door.gd
+++ Scenes/Scripts/door.gd
@@ -9,5 +9,6 @@ func _ready() -> void:
 ## If the player enters, the door and the key disappears. This will be changed as soon as I get the assets!
 func _body_entered(body: Node2D):
 	if body.name == "Player":
-		associated_key.queue_free()
 		queue_free()
+		if associated_key:
+			associated_key.queue_free()
